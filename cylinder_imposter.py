e0256fa356d13bc78114849e4382f4bf39f7b931
Erik den Dekker <erikdendekker@qcode.eu>
2024-03-11 23:36:57 +0100

    Replicated Sidney commit 22c0f26bb5ed012ac44b4258ed9ab5f24d625ff1 - Added Phong shading to Cylinder imposter

--- cylinder_imposter.py
+++ cylinder_imposter.py
@@ -55,6 +55,10 @@ uniform mat4 model_matrix;
 uniform mat4 view_matrix;
 uniform mat4 projection_matrix;
 
+uniform mat4 view_model_matrix;
+uniform mat4 transposed_inverse_view_model_matrix;
+uniform mat4 transposed_inverse_view_matrix;
+
 uniform sampler2D my_texture;
 
 const float pi = 4 * atan(1);
@@ -64,7 +68,8 @@ const float nan = 0.0 / 0.0;
 float intersect_unit_cylinder(vec2 origin, vec2 direction)
 {
     // See: https://en.wikipedia.org/wiki/Line–sphere_intersection
-    // Find smallest real alpha such that: origin + alpha * direction is on the unit circle
+    // Find smallest real alpha such that: origin + alpha * direction is on the unit cylinder
+    // The unit-cylinder stretched for -inf to +inf in the Z direction
     float oo = dot(origin, origin);
     float uo = dot(direction, origin);
     float uu = dot(direction, direction);
@@ -78,6 +83,14 @@ float intersect_unit_cylinder(vec2 origin, vec2 direction)
 
     return (-uo - sqrt(discriminant)) / uu;
 }
+
+
+// Phong constants
+const float ia = 0.2;
+const float id1 = 0.6;
+const float is1 = 1.02;
+
+const float phong_alpha = 20.0;
 
 
 void main()
@@ -121,15 +134,39 @@ void main()
         discard;
     }
 
+    // Find texture coordinates
     float u = 0.5 + 0.5 * atan(cylinder_hit.x, cylinder_hit.y) / pi;
     float v = 0.5 + cylinder_hit.z;
 
-    fragment_color = texture(my_texture, vec2(u, v));
+    vec3 k_material = texture(my_texture, vec2(u, v)).xyz;
 
-    vec4 proj = projection_matrix * view_matrix * model_matrix * vec4(cylinder_hit, 1);
-    float depth = (proj.z / proj.w);
+    // Fix depth
+    vec4 projection = projection_matrix * view_matrix * model_matrix * vec4(cylinder_hit, 1);
+    gl_FragDepth = 0.5 + 0.5 * (projection.z / projection.w);
 
-    gl_FragDepth = 0.5 + 0.5 * depth;
+    // Do phong shading
+    vec3 mv_surface = (view_model_matrix * vec4(cylinder_hit, 1)).xyz;
+    vec3 mv_surface_normal = normalize((transposed_inverse_view_model_matrix * vec4(cylinder_hit, 1)).xyz);
+    vec3 mv_viewer_direction = normalize(-mv_surface);
+
+    vec3 m_lightsource1_direction = normalize(vec3(+1, 1, 1));
+    vec3 mv_lightsource1_direction = normalize((transposed_inverse_view_matrix * vec4(m_lightsource1_direction, 0)).xyz);
+    vec3 mv_lightsource1_reflection_direction = 2 * dot(mv_lightsource1_direction, mv_surface_normal) * mv_surface_normal - mv_lightsource1_direction;
+
+//  vec3 m_lightsource2_direction = normalize(vec3(-1, 1, 1));
+//  vec3 mv_lightsource2_direction = normalize((transposed_inverse_view_matrix * vec4(m_lightsource2_direction, 0)).xyz);
+//  vec3 mv_lightsource2_reflection_direction = 2 * dot(mv_lightsource2_direction, mv_surface_normal) * mv_surface_normal - mv_lightsource2_direction;
+
+    float contrib_d1 = max(0.0, dot(mv_lightsource1_direction, mv_surface_normal));
+    float contrib_s1 = pow(max(0.0, dot(mv_lightsource1_reflection_direction, mv_viewer_direction)), phong_alpha);
+
+//  float contrib_d2 = max(0.0, dot(mv_lightsource2_direction, mv_surface_normal));
+//  float contrib_s2 = pow(max(0.0, dot(mv_lightsource2_reflection_direction, mv_viewer_direction)), alpha);
+
+    vec3 phong_color = k_material * (ia + id1 * contrib_d1 + is1 * contrib_s1);
+//  vec3 phong_color = k_material * (ia + id1 * contrib_d1 + is1 * contrib_s1 + id2 * contrib_d2 + is2 * contrib_s2);
+
+    fragment_color = vec4(phong_color, 1.0);
 }
 '''
 
@@ -141,9 +178,16 @@ class CylinderImpostor(Renderable):
         (self._shaders, self._shader_program) = create_opengl_program(vertex_shader=vertex_shader,
                                                                       fragment_shader=fragment_shader)
 
-        self._model_matrix_location      = glGetUniformLocation(self._shader_program, "model_matrix")
-        self._view_matrix_location       = glGetUniformLocation(self._shader_program, "view_matrix")
-        self._projection_matrix_location = glGetUniformLocation(self._shader_program, "projection_matrix")
+        self._model_matrix_location                                    = glGetUniformLocation(self._shader_program, "model_matrix")
+        self._view_matrix_location                                     = glGetUniformLocation(self._shader_program, "view_matrix")
+        self._projection_matrix_location                               = glGetUniformLocation(self._shader_program, "projection_matrix")
+
+        self._view_model_matrix_location                               = glGetUniformLocation(self._shader_program, "view_model_matrix")
+        self._projection_view_model_matrix_location                    = glGetUniformLocation(self._shader_program, "projection_view_model_matrix")
+        self._inverse_view_model_matrix_location                       = glGetUniformLocation(self._shader_program, "inverse_view_model_matrix")
+        self._transposed_inverse_view_matrix_location                  = glGetUniformLocation(self._shader_program, "transposed_inverse_view_matrix")
+        self._transposed_inverse_view_model_matrix_location            = glGetUniformLocation(self._shader_program, "transposed_inverse_view_model_matrix")
+        self._transposed_inverse_projection_view_model_matrix_location = glGetUniformLocation(self._shader_program, "transposed_inverse_projection_view_model_matrix")
 
         self._texture = glGenTextures(1)
         glBindTexture(GL_TEXTURE_2D, self._texture)
@@ -225,9 +269,17 @@ class CylinderImpostor(Renderable):
     def render(self, projection_matrix, view_matrix, model_matrix):
         glUseProgram(self._shader_program)
 
-        glUniformMatrix4fv(self._model_matrix_location,      1, GL_TRUE, model_matrix     .astype(np.float32))
-        glUniformMatrix4fv(self._view_matrix_location,       1, GL_TRUE, view_matrix      .astype(np.float32))
-        glUniformMatrix4fv(self._projection_matrix_location, 1, GL_TRUE, projection_matrix.astype(np.float32))
+        glUniformMatrix4fv(self._model_matrix_location,                                    1, GL_TRUE, model_matrix     .astype(np.float32))
+        glUniformMatrix4fv(self._view_matrix_location,                                     1, GL_TRUE, view_matrix      .astype(np.float32))
+        glUniformMatrix4fv(self._projection_matrix_location,                               1, GL_TRUE, projection_matrix.astype(np.float32))
+
+        # Precompute
+        glUniformMatrix4fv(self._view_model_matrix_location,                               1, GL_TRUE,                                  (view_matrix @ model_matrix)  .astype(np.float32))
+        glUniformMatrix4fv(self._projection_view_model_matrix_location,                    1, GL_TRUE,              (projection_matrix @ view_matrix @ model_matrix)  .astype(np.float32))
+        glUniformMatrix4fv(self._inverse_view_model_matrix_location,                       1, GL_TRUE, np.linalg.inv(                    view_matrix @ model_matrix)  .astype(np.float32))
+        glUniformMatrix4fv(self._transposed_inverse_view_matrix_location,                  1, GL_TRUE, np.linalg.inv(                    view_matrix               ).T.astype(np.float32))
+        glUniformMatrix4fv(self._transposed_inverse_view_model_matrix_location,            1, GL_TRUE, np.linalg.inv(                    view_matrix @ model_matrix).T.astype(np.float32))
+        glUniformMatrix4fv(self._transposed_inverse_projection_view_model_matrix_location, 1, GL_TRUE, np.linalg.inv(projection_matrix @ view_matrix @ model_matrix).T.astype(np.float32))
 
         glBindTexture(GL_TEXTURE_2D, self._texture)
         glBindVertexArray(self._vao)
